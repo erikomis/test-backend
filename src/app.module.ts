990b092beba4ad4d5294b792bac128f28e24b521
erikmartins <erik.ozzetti.martins@gmail.com>
2023-03-03 21:45:41 -0300

    fix module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { DatabaseModule } from './database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
